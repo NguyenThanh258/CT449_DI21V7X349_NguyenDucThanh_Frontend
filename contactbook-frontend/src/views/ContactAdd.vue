f4fb53f836e3801aedb16a9f6d63512d523817a5
NguyenThanh258 <nguyenthanh537562@gmail.com>
2024-01-08 05:02:03 +0700

    Nop Frontend

---
+++ contactbook-frontend/src/views/ContactAdd.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="page">
+        <h4>Thêm mới Liên hệ</h4>
+        <ContactForm :contact="newContact" @onSubmit="addContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+    import ContactForm from "@/components/ContactForm.vue";
+    import ContactService from "@/services/contact.service";
+    export default {
+        components: {ContactForm,
+        },
+        data() {
+            return {
+                message: "",
+                newContact: {}                
+            };
+        },
+        methods: {
+            async addContact(data) {
+                try {
+                    await ContactService.create(data);
+                    alert('Liên hệ được thêm mới thành công.');
+                    this.$router.push({ name: "contactbook" });
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+        },
+    };
+</script>
